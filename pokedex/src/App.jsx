68ea59803536461d6abc0e45858f725771c48a3b
Gustavo <gustavo.mtz.df@gmail.com>
2023-08-23 01:49:51 -0500

    se agregan funciones para recuperar contenido adicional del api de pokemones

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemones, setPokemones]= useState([]);
+
+  useEffect(()=>{
+    const getPokemones = async()=>{
+      const response = await fetch ('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
+      const listapokemones = await response.json()
+      const {results} = listapokemones
+      const newPokemones = results.map(async(pokemon)=>{
+        
+        const response = await fetch(pokemon.url)
+        const poke = await response.json()
+
+        return {
+          id : poke.id,
+          name : poke.name,
+          img: poke.sprites.other.dream_world.front_default
+          
+        }
+      })
+
+      setPokemones(await Promise.all(newPokemones))
+    }
+  
+    getPokemones();
+    
+  }, [])
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <h1>Pokedex</h1>
+
+        {
+            pokemones.map(pokemon => <p>{pokemon.name}</p>)
+
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
