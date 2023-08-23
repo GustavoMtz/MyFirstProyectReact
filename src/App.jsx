f616f21db42241e1cdca90854b9adb611ec57b95
Gustavo <gustavo.mtz.df@gmail.com>
2023-08-23 00:03:33 -0500

    se agrega context con el tema de la aplicacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import {React,  useState } from "react";
 import "./App.css";
 import Pokemones from "./components/Pokemones";
 
 
 const App = ()=>{
-    return <div className="container">
+ 
+    const [theme, setTheme] = useState['light']
+    const handleTheme =(e)=>{
+        setTheme(e.target.value)
+    }
+
+
+    return (
+    
+    <>
+    <div className={`text-center py-1 my-1 ${theme}`}>
+        <input type="radio" name="theme" id="light" value="light" onClick={handleTheme}/>
+        <label htmlFor="light" className="mx-1">Claro</label>
+        <input type="radio" name="theme" id="dark" value="dark" onClick={handleTheme}/>
+        <label htmlFor="dark" className="mx-1">Oscuro</label>
+    </div>
+     <div className={`container-fluid p-3 d-flex flex-row  ${theme}`}>
            
            
-            <Pokemones/>
+           <Pokemones/>
+          
            
-            
-    </div>  
+   </div>  
+    
+    </>
+   
+    )
 }
 
 export default App;
---
+++ src/components/ThemeContex.jsx
@@ -0,0 +1,10 @@
+const  ThemeContext = ({theme})=>{
+    return (
+        <div className={theme}>
+            <h1>Tema </h1>
+        </div>
+
+    )
+}
+
+export default ThemeContext
