33c7170c90c44299f6772c5cb0f10a852506532f
Gustavo <gustavo.mtz.df@gmail.com>
2023-08-23 04:19:23 -0500

    se agregan funciones para cargar mas pokemones y una url default al iniciar la aplicacion

--- pokedex/src/hooks/usePokemones.jsx
+++ pokedex/src/hooks/usePokemones.jsx
@@ -1,35 +1,55 @@
 import { useState, useEffect } from 'react'
+const URL_DEFAULT='https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
 
 function usePokemones(){
     const [pokemones, setPokemones]= useState([]);
+    const [siguienteUrl, setSiguienteUrl] = useState('');
 
-    useEffect(()=>{
-        const getPokemones = async()=>{
-          const response = await fetch ('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
-          const listapokemones = await response.json()
-          const {results} = listapokemones
-          const newPokemones = results.map(async(pokemon)=>{
-            
-            const response = await fetch(pokemon.url)
-            const poke = await response.json()
-    
-            return {
-              id : poke.id,
-              name : poke.name,
-              img: poke.sprites.other.dream_world.front_default
-              
-            }
-          })
-    
-          setPokemones(await Promise.all(newPokemones))
-        }
-      
-        getPokemones();
+    const getPokemones = async(url = URL_DEFAULT)=>{
+        const response = await fetch (url)
+        const listapokemones = await response.json()
+        const {next, results} = listapokemones
+       
+        const newPokemones = Promise.all(
+            results.map(async(pokemon)=>{
+          
+                const response = await fetch(pokemon.url)
+                const poke = await response.json()
         
+                return {
+                  id : poke.id,
+                  name : poke.name,
+                  img: poke.sprites.other.dream_world.front_default
+                  
+                }
+              })
+        )
+           
+        
+
+
+        return {next, newPokemones}
+        
+      }
+
+    const obtenerPokemones = async ()=>{
+        const {next, newPokemones} = await getPokemones()
+        setPokemones(newPokemones)
+        setSiguienteUrl(next)
+    }
+
+    const masPokemones = async ()=>{
+        const {next, newPokemones} = await getPokemones(siguienteUrl)
+        setPokemones(prev => [...prev , ...newPokemones])
+        setSiguienteUrl(next)
+    }
+
+    useEffect(()=>{   
+        obtenerPokemones();  
       }, [])
     
 
-      return {pokemones}
+      return {pokemones, masPokemones}
 
 }
 
