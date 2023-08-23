e5d02cef522c32e24ef38f4fd6e005637d412f01
Gustavo <gustavo.mtz.df@gmail.com>
2023-08-22 23:29:14 -0500

    Se agrega useMemo para evitar re carga de operaciones pesadas durante el renderizado

--- src/components/MemoHijo.jsx
+++ src/components/MemoHijo.jsx
@@ -2,13 +2,35 @@ import {memo} from 'react'
 
 
 //impide que se vuelva a renderizar el componente hijo, queda en cache
-const MemoHijo = ({contador}) =>{
+const MemoHijo = ({contador, sumar, restar}) =>{
+
+//let maxnumero = 0
+
+//for(let i=0;i<  100000000; i++){
+//    maxnumero++;
+//}
+
+const maxnumero = useMemo(()=> {
+    let maxnumero = 0
+
+    for(let i=0;i<  100000000; i++){
+        maxnumero++;
+    }
+    return maxnumero
+}, [])
+
+//useMemo permite guardar un valor retornado por una funcion y no va a ser ejecutado a menos que el arreglo de dependencias lo pida
+// en el ejemplo solo lo hace la primera vez que carga por ser procesos pesados o valores que no van a cambiar muy seguido
 
     console.log('Renderizando Hijo');
     return (
         <div>
             <h1>Memo Hijo</h1>
             <h3>{contador}</h3>
+            <button onClick={sumar}> + </button>
+            <button onClick={restar}> - </button>
+
+            <h3>{maxnumero}</h3>
         </div>        
     )
 
--- src/components/Memorizacion.jsx
+++ src/components/Memorizacion.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import MemoHijo from './Memohijo';
 
 
 const Memorizacion = ()=>{
     const [contador, setContador] = useState(0);
     const [text, setText] = useState("");
-    const sumar = ()=>{setContador(contador+1)}
-    const restar = ()=>{setContador(contador-1)}
+    //const sumar = ()=>{setContador(contador+1)}
+    //const restar = ()=>{setContador(contador-1)}
 
+    const sumar = useCallback(()=>{setContador(contador+1)}, [contador])
+    const restar = useCallback(()=>{setContador(contador-1)}, [contador])
+//useCallback memoriza la funcion y solo se ejecuta cuando el arbol de dependencias (en este caso contador) se actualiza
     const handleChange = (e) => setText(e.target.value)
 
     return (
@@ -22,7 +25,8 @@ const Memorizacion = ()=>{
 
             <input type="text" onChange={handleChange} value={text}/>
 
-            <MemoHijo contador ={contador}  sumar={sumar}  restar={restar}/>
+            <MemoHijo contador ={contador}  sumar={sumar}  restar={restar}/> 
+            
         </div>
     )
 }
